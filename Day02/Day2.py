ccb23e77b2cc06f950fadda3e8fc8d653391a4af
a-wieczorek <102622810+a-wieczorek@users.noreply.github.com>
2024-12-02 12:31:07 +0100

    Day2

--- Day02/Day2.py
+++ Day02/Day2.py
@@ -13,12 +13,14 @@ def analyse_reports(data: list) -> (int, list):
             curr = row[i]
             if not 1 <= abs(int(prev) - int(curr)) <= 3:
                 isSafe = False
+                retryRows.append(row[:max(i - 2, 0)] + row[i - 1:])
                 retryRows.append(row[:i-1] + row[i:])
                 retryRows.append(row[:i] + row[i+1:])
                 break
             if curr < prev:
                 if state == 'increasing':
                     isSafe = False
+                    retryRows.append(row[:max(i - 2, 0)] + row[i - 1:])
                     retryRows.append(row[:i - 1] + row[i:])
                     retryRows.append(row[:i] + row[i+1:])
                     break
@@ -26,6 +28,7 @@ def analyse_reports(data: list) -> (int, list):
             else:
                 if state == 'decreasing':
                     isSafe = False
+                    retryRows.append(row[:max(i - 2, 0)] + row[i - 1:])
                     retryRows.append(row[:i - 1] + row[i:])
                     retryRows.append(row[:i] + row[i+1:])
                     break
@@ -41,6 +44,6 @@ def analyse_reports(data: list) -> (int, list):
 res, retry = analyse_reports(data)
 print(f'{sum(res)}')
 res2, _ = analyse_reports(retry)
-out = [max(res2[i:i + 2]) for i in range(0, len(res2), 2)]
+out = [max(res2[i:i + 3]) for i in range(0, len(res2), 3)]
 print(f'{sum(res) + sum(out)}')
 
